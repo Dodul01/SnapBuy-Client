04d4b5c5713e8460cae86f40db9cae34c016f6a6
Dodul01 <mhdodul299@gmail.com>
2025-02-14 12:17:57 +0600

    Project setup complete

---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to SnapBuy</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/assets/svgs/Logo.tsx
@@ -0,0 +1,37 @@
+// export default function Logo() {
+//     return (
+//       <svg
+//         id="logo-72"
+//         width="35"
+//         height="24"
+//         viewBox="0 0 60 40"
+//         fill="none"
+//         xmlns="http://www.w3.org/2000/svg"
+//       >
+//         <path
+//           d="M23.2997 0L52.0461 28.6301V44H38.6311V34.1553L17.7522 13.3607L13.415 13.3607L13.415 44H0L0 0L23.2997 0ZM38.6311 15.2694V0L52.0461 0V15.2694L38.6311 15.2694Z"
+//           className="ccustom"
+//           fill="#212326"
+//         ></path>
+//       </svg>
+//     );
+//   }
+export default function Logo() {
+  return (
+    <svg
+      width="35"
+      height="24"
+      viewBox="0 0 60 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15 10C15 5 20 2 25 2H35C40 2 45 5 45 10C45 15 40 17 35 17H25C20 17 15 20 15 25C15 30 20 33 25 33H35C40 33 45 30 45 25"
+        stroke="#212326"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
