6b5b43f9d9c448319631ae4de251fcb04eeedd79
Dodul01 <mhdodul299@gmail.com>
2025-02-14 12:31:58 +0600

    registration page added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,11 @@
+import RegisterForm from "@/components/modules/auth/register/RegisterForm";
+
 const RegisterPage = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default RegisterPage;
