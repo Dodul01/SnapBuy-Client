0cf038754126b0a020411d20a02117378ca39e2d
Dodul01 <mhdodul299@gmail.com>
2025-02-17 14:45:57 +0600

    Captch and user context added

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { useUser } from "@/context/UserContext";
 
 const HomePage = () => {
+  const user = useUser();
+  console.log(user);
+
   return (
     <div>
       <h1>Welcome to SnapBuy</h1>
